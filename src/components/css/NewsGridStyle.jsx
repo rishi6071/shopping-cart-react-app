478b3a4d09abf9e13d7c00324119561ce6c897a4
Rishi Soni <rishisoni6071@gmail.com>
2021-08-29 19:50:07 +0530

    Performance Improvement on NewsGrid

--- src/components/css/NewsGridStyle.jsx
+++ src/components/css/NewsGridStyle.jsx
@@ -1,6 +1,6 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     marginRight: 20,
     marginLeft: 20,
@@ -28,6 +28,27 @@ const useStyles = makeStyles({
       color: "white",
     },
   },
-});
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+  avatar: {
+    backgroundColor: "black",
+  },
+  newsContentBox: {
+    height: 195,
+    overflow: "hidden",
+  },
+}));
 
 export default useStyles;
