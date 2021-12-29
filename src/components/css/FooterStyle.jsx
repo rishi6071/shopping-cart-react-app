05bf53ef7a3d23ec0e26ce6ed202e8a0e6ad8cd0
Rishi Soni <rishisoni6071@gmail.com>
2021-12-30 01:03:24 +0530

    Added Subscribe Letter in Footer

--- src/components/css/FooterStyle.jsx
+++ src/components/css/FooterStyle.jsx
@@ -1,7 +1,74 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-  footerDarkBox: {
+  subscribeGridBox: {
+    borderTop: "1.5px solid #ebebeb",
+    padding: "50px 35px",
+    paddingTop: 40,
+    fontFamily: "Archivo, sans-serif",
+    [theme.breakpoints.down("sm")]: {
+      padding: "50px 5px",
+      paddingTop: 30,
+      textAlign: "center",
+    },
+  },
+  subscribeGridItem: {
+    marginTop: 15,
+    paddingLeft: 30,
+    paddingRight: 30,
+    [theme.breakpoints.down("sm")]: {
+      paddingLeft: 10,
+      paddingRight: 10,
+    },
+  },
+  subscribeHeading: {
+    textTransform: "uppercase",
+    fontFamily: "Archivo, sans-serif",
+  },
+  subscribeSubHeading: {
+    color: "#808D9E",
+    fontFamily: "Archivo, sans-serif",
+    fontSize: "14px",
+    marginTop: 4,
+    [theme.breakpoints.down("sm")]: {
+      marginTop: 15,
+    },
+  },
+  subscribeInput: {
+    boxSizing: "border-box",
+    border: "none",
+    background: "#F1F1F1",
+    padding: "14px 25px",
+    fontSize: "14px",
+    width: "100%",
+    "&::placeholder": {
+      fontFamily: "Archivo, sans-serif",
+      fontSize: "14.3px",
+    },
+    "&:focus": {
+      outline: "none",
+    },
+    [theme.breakpoints.down("sm")]: {
+      marginTop: 15,
+    },
+  },
+  subscribeBtn: {
+    padding: "9px 25px",
+    background: "#222222",
+    color: "#FFFFFF",
+    borderRadius: 0,
+    textTransform: "lowercase",
+    width: "100%",
+    fontSize: "15px",
+    border: "none",
+    "&:hover": {
+      background: "#000000",
+    },
+    [theme.breakpoints.down("sm")]: {
+      marginTop: 15,
+    },
+  },
+  footerGridBox: {
     paddingTop: 25,
     paddingBottom: 25,
     display: "flex",
--- src/components/sectionComponents/Footer.jsx
+++ src/components/sectionComponents/Footer.jsx
@@ -1,22 +1,51 @@
 import React from "react";
 import useStyles from "../css/FooterStyle";
-import '../css/style.css';
+import "../css/style.css";
 
 // Material-UI Components
-import { Grid } from "@material-ui/core";
+import { Grid, Typography, Button } from "@material-ui/core";
 
 const Footer = () => {
   const classes = useStyles();
 
   return (
-    <>
-      <footer>
-        {/* Dark Section */}
-        <Grid className={classes.footerDarkBox}>
-          Copyright © 2021 By Commerce.js
+    <footer>
+      {/* Subscribe Section */}
+      <Grid container className={classes.subscribeGridBox}>
+        <Grid item md={6} sm={12} className={classes.subscribeGridItem}>
+          <Typography variant="h5" className={classes.subscribeHeading}>
+            Subscribe to our NewsLetter
+          </Typography>
+          <Typography className={classes.subscribeSubHeading}>
+            A short sentence describing what someone will receive by subscribing
+          </Typography>
         </Grid>
-      </footer>
-    </>
+        <Grid item md={6} sm={12} className={classes.subscribeGridItem}>
+          <Grid
+            container
+            justifyContent="center"
+            alignItems="center"
+            className={classes.subscribeInputGroup}
+          >
+            <Grid item sm={8}>
+              <input
+                type="email"
+                className={classes.subscribeInput}
+                placeholder="Enter your email address"
+              />
+            </Grid>
+            <Grid item sm={4}>
+              <Button className={classes.subscribeBtn}>Subscribe</Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      {/* Footer Section */}
+      <Grid container className={classes.footerGridBox}>
+        Copyright © 2021 By Commerce.js
+      </Grid>
+    </footer>
   );
 };
 
